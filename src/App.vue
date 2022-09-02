744177f626d2e7be4427e1a9901447f37de0e416
MisakiYuuko <1035996529@qq.com>
2022-09-02 19:31:14 +0800

    第四天

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- 登录页面，layout -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import { loginAPI } from './api/index.js'
+
+export default {
+  name: 'App',
+
+  methods: {
+    // async await try catch
+    // async login () {
+    //   try {
+    //     const res = await loginAPI()
+    //     console.log(res)
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    // }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.demo {
+  width: 400px;
+  padding: 20px;
+  border: 1px solid red;
+}
+</style>
